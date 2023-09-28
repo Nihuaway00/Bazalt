d864094304dc80bcc169d0c5808547d020e3a442
Nikita Troshin <nikitaemaeny@gmail.com>
2023-09-28 10:56:03 +0300

    firebase became local

--- backend/src/database/firebase.js
+++ backend/src/database/firebase.js
@@ -1,18 +1,11 @@
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"
 import { initializeApp } from "firebase/app"
-import init from "../../firebase.init.js"
 
-
-if (!init) {
-	console.log("ERROR: firebase\nMESSAGE: config file not found \n")
-	console.log("You should initialize firebase!")
-	console.log("To do this you should add firebase.init.json to root dir")
-	console.log("Where will the config")
-	process.exit(1)
-}
-
-
-initializeApp(init)
+initializeApp({
+	"apiKey": "gfh45hrtkjtykt6t", //not valid apikey, but it works locally
+	"projectId": "bazalt",
+	"storageBucket": "bazalt",
+})
 
 const db = getFirestore()
 connectFirestoreEmulator(db, "firebase", 8080)
