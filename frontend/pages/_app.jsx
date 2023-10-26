ebc380a77e4c679300ca0c91d917225b756314c8
Nihuaway <nikitatroshin2305@gmail.com>
2023-10-26 11:02:53 +0300

    functionality for keep authorize

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,5 @@
 
-import { Provider, useDispatch } from 'react-redux'
+import { Provider, useDispatch, useSelector } from 'react-redux'
 import RootLayout from '../layouts/layout'
 import { store } from '../store/store'
 import '../styles/global.css'
@@ -10,6 +10,7 @@ import { io } from 'socket.io-client'
 import { socketConnect } from '../store/slices/socketSlice'
 import { Providers } from '../layouts/providers'
 import { Middlewares } from '../layouts/middlewares'
+import { AesCryptoHandler } from '../handlers/cryptoHandler'
 
 
 export default function MyApp({ Component, pageProps }) {
@@ -30,6 +31,20 @@ export default function MyApp({ Component, pageProps }) {
 
 		const socket = io("http://localhost:8000", {
 			extraHeaders: { sessionID }
+		})
+
+		socket.on('SERVER:message', async ({ encrypted, iv, chatID }) => {
+
+			const chatKeys = store.getState().chatKeys
+			const chatKey = chatKeys.filter(chatKey => chatKey.chatID === chatID)[0]
+			const aes = new AesCryptoHandler()
+			await aes.importKey('jwk', chatKey.key)
+			const decrypted = await aes.decrypt(
+				new Uint8Array(encrypted.split(",")),
+				new Uint8Array(iv.split(","))
+			)
+			const data = JSON.parse(decrypted)
+			console.log(data);
 		})
 	}, [])
 
