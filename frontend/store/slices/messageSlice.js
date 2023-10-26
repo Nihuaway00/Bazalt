1e4cdab2807e506f02ed18aaad38ef54d8617b24
Nihuaway <nikitatroshin2305@gmail.com>
2023-10-26 10:59:55 +0300

    Added store for chat keys

---
+++ frontend/store/slices/messageSlice.js
@@ -0,0 +1,15 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// export const MessageSlice = createSlice({
+// 	name: 'messages',
+// 	initialState: null,
+// 	reducers:{
+// 		add: (state, payload) => {
+
+// 		},
+// 		remove: (state, payload) => {
+
+// 		},
+
+// 	}
+// })
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from './slices/userSlice'
 import socketReducer from './slices/socketSlice'
+import chatKeyReducer from './slices/chatKeySlice'
 
 const store = configureStore({
 	reducer: {
 		user: userReducer,
-		socket: socketReducer
+		socket: socketReducer,
+		chatKeys: chatKeyReducer
 	},
 	devTools: true
 })
